88a4b903498de6bfcf15b605d7b03dee991bc6e6
Maxim Shchenkov <goodok61@gmail.com>
2020-03-12 22:37:21 +0300

    Урок 19

--- script/script.js
+++ script/script.js
@@ -1,13 +1,13 @@
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('DOMContentLoaded', () => {
   'use strict'
 
-  function countTimer(deadline) {
-    let timerHours = document.querySelector('#timer-hours'),
+  const countTimer = (deadline) => {
+    const timerHours = document.querySelector('#timer-hours'),
       timerMinutes = document.querySelector('#timer-minutes'),
       timerSeconds = document.querySelector('#timer-seconds');
 
-    function getTimeRemaining() {
-      let dateStop = new Date(deadline).getTime(),
+    const getTimeRemaining = () => {
+      const dateStop = new Date(deadline).getTime(),
         dateNow = new Date().getTime(),
         timeRemaining = (dateStop - dateNow) / 1000,
         seconds = Math.floor(timeRemaining % 60),
@@ -21,8 +21,8 @@ window.addEventListener('DOMContentLoaded', function () {
       };
     }
 
-    function updateClock() {
-      let timer = getTimeRemaining();
+    const updateClock = () => {
+      const timer = getTimeRemaining();
 
       timerHours.textContent = timer.hours < 10 ? "0" + timer.hours : timer.hours;
       timerMinutes.textContent = timer.minutes < 10 ? "0" + timer.minutes : timer.minutes;
@@ -34,37 +34,36 @@ window.addEventListener('DOMContentLoaded', function () {
         timerSeconds.textContent = '00';
       }
     }
-
+    updateClock();
     setInterval(updateClock, 1000);
 
   }
-
-  countTimer('20 mart 2020');
+  countTimer('20 march 2020');
 
   const toogleMenu = () => {
     const btnMenu = document.querySelector('.menu'),
       menu = document.querySelector('menu'),
-      closeBtn = document.querySelector('.close-btn'),
-      menuItems = menu.querySelectorAll('ul>li>a');
+      handlerMenu = () => menu.classList.toggle('active-menu');
 
-    const handlerMenu = () => menu.classList.toggle('active-menu');
+    menu.addEventListener('click', (event) => {
+      let {target} = event;
+
+      if (target.closest('a')) handlerMenu();
+    });
+
     btnMenu.addEventListener('click', handlerMenu);
-    closeBtn.addEventListener("click", handlerMenu);
-
-    menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
   }
   toogleMenu();
 
   //popup
 
   const tooglePopUp = () => {
-    const popup = document.querySelector(".popup"),
-      popupBtn = document.querySelectorAll(".popup-btn"),
-      popUpClose = document.querySelector(".popup-close"),
+    const popUp = document.querySelector(".popup"),
+      popUpBtn = document.querySelectorAll(".popup-btn"),
       popUpContent = document.querySelector(".popup-content");
 
-    popupBtn.forEach((elem) => elem.addEventListener('click', () => {
-      popup.style.display = "block";
+    popUpBtn.forEach((elem) => elem.addEventListener('click', () => {
+      popUp.style.display = "block";
       popUpContent.style.opacity = "0";
 
 
@@ -75,18 +74,31 @@ window.addEventListener('DOMContentLoaded', function () {
             return timeFraction;
           },
           draw: function (progress) {
-            let progressPopup = Math.floor(progress * 100) / 100;
-            popUpContent.style.opacity = `${progressPopup}`;
+            let progressPopUp = Math.floor(progress * 100) / 100;
+            popUpContent.style.opacity = `${progressPopUp}`;
           }
         });
       } else {
-        popup.style.display = "block";
+        popUp.style.display = "block";
         popUpContent.style.opacity = "1";
       }
 
 
     }));
-    popUpClose.addEventListener('click', () => popup.style.display = "none");
+
+    popUp.addEventListener('click', (event) => {
+      let target = event.target;
+
+      if (target.classList.contains('popup-close')) {
+        popUp.style.display = "none";
+      } else {
+        target = target.closest('.popup-content');
+        if (!target) {
+          popUp.style.display = "none";
+        }
+      }
+
+    })
 
     function animate({
       timing,
@@ -96,14 +108,12 @@ window.addEventListener('DOMContentLoaded', function () {
       let start = performance.now();
 
       requestAnimationFrame(function animate(time) {
-        // timeFraction изменяется от 0 до 1
         let timeFraction = (time - start) / duration;
         if (timeFraction > 1) timeFraction = 1;
 
-        // вычисление текущего состояния анимации
         let progress = timing(timeFraction);
 
-        draw(progress); // отрисовать её
+        draw(progress);
 
         if (timeFraction < 1) {
           requestAnimationFrame(animate);
@@ -113,4 +123,38 @@ window.addEventListener('DOMContentLoaded', function () {
   };
   tooglePopUp();
 
+  //табы
+
+  const tabs = () => {
+    const tabHeader = document.querySelector('.service-header'),
+      tab = tabHeader.querySelectorAll('.service-header-tab'),
+      tabContent = document.querySelectorAll('.service-tab');
+    const toogleTabContent = (index => {
+      for (let i = 0; i < tabContent.length; i++) {
+        if (index === i) {
+          tab[i].classList.add('active');
+          tabContent[i].classList.remove('d-none');
+        } else {
+          tab[i].classList.remove('active');
+          tabContent[i].classList.add("d-none");
+        }
+
+      }
+    })
+    tabHeader.addEventListener('click', (event) => {
+      let target = event.target;
+      target = target.closest('.service-header-tab');
+
+      if (target) {
+        tab.forEach((item, i) => {
+          if (item === target) {
+            toogleTabContent(i);
+
+          }
+        })
+      }
+    })
+
+  }
+  tabs();
 });
